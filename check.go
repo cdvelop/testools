fd0da5bae4b0045e4937412f77edcbdad8a73508
Cesar Solis <44058491+cdvelop@users.noreply.github.com>
2023-11-16 17:12:40 -0300

    refactor refactor CheckTest

--- check.go
+++ check.go
@@ -3,20 +3,16 @@ package testools
 import (
 	"fmt"
 	"reflect"
-	"testing"
 )
 
-func CheckTest(prueba string, expected, response interface{}, t *testing.T, a ...any) {
+func (r Request) CheckTest(expected, response interface{}, comment ...any) bool {
 
 	if !reflect.DeepEqual(expected, response) {
-		fmt.Println(a...)
-		// if expected != response {
-		fmt.Println("=>PRUEBA: ", prueba)
+		fmt.Println("=>PRUEBA: ", r.TestName)
 		fmt.Printf("=>SE ESPERABA:[%v]\n\n=>SE OBTUVO:[%v]\n", expected, response)
-		t.Fatal()
+		fmt.Println(comment...)
+		return false
 	}
-}
 
-func (r Request) CheckTest(expected, response interface{}, comment ...any) {
-	CheckTest(r.TestName, expected, response, r.T, comment...)
+	return true
 }
